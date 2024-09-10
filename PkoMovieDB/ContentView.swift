d7a7e62ee7d0b66da2d4c8ae007082ad4a93fe71
Maciej Szostak <maciek@recostudio.pl>
2024-09-10 14:53:07 +0200

    Initial Commit

---
+++ PkoMovieDB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PkoMovieDB
+//
+//  Created by Maciej Szostak on 10/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PkoMovieDB/PkoMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  PkoMovieDBApp.swift
+//  PkoMovieDB
+//
+//  Created by Maciej Szostak on 10/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PkoMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
